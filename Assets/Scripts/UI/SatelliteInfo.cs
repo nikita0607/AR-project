eda6b94bbf8656e2d9ca456a39d70c28382b73c6
ArMaGeDoN <ilyakur025@gmail.com>
2025-04-02 21:26:08 +0300

    фикс трейлов

--- Assets/Scripts/UI/SatelliteInfo.cs
+++ Assets/Scripts/UI/SatelliteInfo.cs
@@ -28,7 +28,8 @@ public class SatelliteInfo : MonoBehaviour
 
         _textField.text = text;
 
-        satellite.timeForSatellite = new EpochTime(DateTime.UtcNow);
+        // satellite.timeForSatellite = new EpochTime(DateTime.UtcNow);
+        satellite.timeForSatellite = TimeManager.Instance.GetTime();
 
         for (int i=0; i<94; i++) {
             Coordinate satCords = satellite.GetPosition();
@@ -45,6 +46,7 @@ public class SatelliteInfo : MonoBehaviour
             };
         };
 
+        TimeManager.Instance.ResetTime();
         satellite.timeForSatellite = TimeManager.Instance.GetTime();
     }
 
