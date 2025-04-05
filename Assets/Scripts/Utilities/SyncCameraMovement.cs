c44fd9f3b01b4e938c59c0d54004847155a917a8
ArMaGeDoN <ilyakur025@gmail.com>
2025-04-06 01:38:08 +0300

    add new cam

---
+++ Assets/Scripts/Utilities/SyncCameraMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public class SyncCameraMovement : MonoBehaviour
+    {
+        public Camera sourceCamera; // Исходная камера, которую нужно копировать
+        private Camera _targetCamera;
+
+        [Header("Настройки синхронизации")]
+        [SerializeField] private bool syncPosition = true;
+        [SerializeField] private bool syncRotation = true;
+        [SerializeField] private bool syncFOV = true;
+        [SerializeField] private bool syncProjection = true;
+
+        void Start()
+        {
+            _targetCamera = GetComponent<Camera>();
+        }
+
+        void LateUpdate()
+        {
+            if (sourceCamera == null) return;
+
+            // Синхронизация позиции и поворота
+            if (syncPosition) 
+                transform.position = sourceCamera.transform.position;
+
+            if (syncRotation) 
+                transform.rotation = sourceCamera.transform.rotation;
+
+            // Синхронизация параметров камеры
+            if (syncFOV) 
+                _targetCamera.fieldOfView = sourceCamera.fieldOfView;
+
+            if (syncProjection)
+            {
+                _targetCamera.orthographic = sourceCamera.orthographic;
+                _targetCamera.orthographicSize = sourceCamera.orthographicSize;
+                _targetCamera.nearClipPlane = sourceCamera.nearClipPlane;
+                _targetCamera.farClipPlane = sourceCamera.farClipPlane;
+            }
+        }
+    }
+}
