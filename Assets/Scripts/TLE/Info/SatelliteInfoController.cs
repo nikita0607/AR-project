b6db6c5468ac4d8b8a3bbbb5b4da7ecd2eba80ad
ArMaGeDoN <ilyakur025@gmail.com>
2025-04-07 00:46:44 +0300

    Fix fonts, place position

--- Assets/Scripts/TLE/Info/SatelliteInfoController.cs
+++ Assets/Scripts/TLE/Info/SatelliteInfoController.cs
@@ -46,7 +46,7 @@ public class SatelliteInfoController : MonoBehaviour
 
     void Update()
     {
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if(Camera.main && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
