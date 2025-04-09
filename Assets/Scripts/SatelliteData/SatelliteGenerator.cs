b13a435e723c336282244e2a718ab38ec301eabc
ArMaGeDoN <ilyakur025@gmail.com>
2025-04-09 13:01:18 +0300

    added russian cubesats

--- Assets/Scripts/SatelliteData/SatelliteGenerator.cs
+++ Assets/Scripts/SatelliteData/SatelliteGenerator.cs
@@ -130,7 +130,7 @@ namespace SatelliteData
             foreach (var specificSatellitePrefab in specificSatellitePrefabList)
             {
                 if (specificSatellitePrefab.GetComponent<Satellite>().Name != loadedTleData.Name) continue;
-
+                
                 Tle parsedLoadedTle = ParserTLE.parseTle(loadedTleData.Line1, loadedTleData.Line2, loadedTleData.Name);
                 GameObject newSatellite = Instantiate(specificSatellitePrefab, parent: satelliteParrent.transform);
                 Satellite newSatelliteComponent = newSatellite.GetComponent<Satellite>();
