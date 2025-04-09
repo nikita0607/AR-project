c9b37eb7761dad2fc0f37a2da98d4e09a9019ae9
nikita0607 <ecfed203@gmail.com>
2025-04-09 16:41:43 +0300

    Add height for satellites

--- Assets/Scripts/UI/SatelliteInfo.cs
+++ Assets/Scripts/UI/SatelliteInfo.cs
@@ -53,7 +53,8 @@ namespace UI
             Coordinate cords = satellite.GetPosition();
             string text = $"Название: {satellite.LoadedTle.getName()}\n" +
                          $"NORAD-ID: {satellite.LoadedTle.getNoradID()}\n" +
-                         $"Координаты:\n\tширота: {cords.getLatitude():f2}\n\tдолгота: {cords.getLongitude():f2}\n";
+                         $"Координаты:\n\tширота: {cords.getLatitude():f2}\n\tдолгота: {cords.getLongitude():f2}\n" + 
+                         $"Высота: {cords.getHeight()}\n";
 
             if (SatellitesSerializer.GetSatellitesSer(satteliteJson).ContainsKey(satellite.Name))
                 text += $"{SatellitesSerializer.GetSatellitesSer(satteliteJson)[satellite.Name].info}\n";
