2f8849fe5eae1e6d115832953668cf623788c12d
ArMaGeDoN <ilyakur025@gmail.com>
2025-04-15 13:15:14 +0300

    update

--- Assets/Scripts/Utilities/PositionFixer.cs
+++ Assets/Scripts/Utilities/PositionFixer.cs
@@ -8,12 +8,19 @@ namespace Utilities
         /// <summary> Исходная позиция объекта. </summary>
         private Vector3 _originalPosition;
         private Quaternion _originalRotation;
+        private Transform _parentTransform;
+
+        private bool _isInitialized = false;
 
         /// <summary> Сохраняет начальную позицию объекта при старте. </summary>
         private void Start()
         {
             _originalPosition = transform.position;
             _originalRotation = transform.rotation;
+            _parentTransform = transform.parent;
+            _isInitialized = true;
+            
+            Debug.Log(_originalPosition.y + " " + name);
         }
 
         /// <summary> Восстанавливает объект в исходную позицию. </summary>
@@ -23,9 +30,13 @@ namespace Utilities
         /// </remarks>
         public void RestorePosition()
         {
-            transform.parent = null;
+            if (!_isInitialized) return;
+            // transform.parent = null;
+            transform.parent = _parentTransform;
             transform.rotation = _originalRotation;
             transform.position = _originalPosition;
         }
+        
+        
     }
 }
